b2ddb9e4f67e2ea11bf46038c949a61fb9196e31
EdgarGlowbo <efqr811@gmail.com>
2024-09-19 21:42:12 -0600

    Attempt on Generic Views. No templates created. ERR: AttributeError: module 'finance_tracking.views' has no attribute 'index'

--- broder_apple/finance_tracking/urls.py
+++ broder_apple/finance_tracking/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("bookentry", views.BookEntryView.as_view(), name="bookentry"),
+    path("account", views.AccountView.as_view(), name="account"),
 ]
--- broder_apple/finance_tracking/views.py
+++ broder_apple/finance_tracking/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
+from .models import Transaction, BookEntry, Account
+from django.views import generic
 
 # Create your views here.
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at index.")
+class IndexView(generic.ListView):
+    template_name = "finance_tracking/index.html"
+    context_object_name = "latest_transaction_list"
+
+    def get_queryset(self):
+        return Transaction.objects.order_by("-transaction_date")[:5]
+
+
+class BookEntryView(generic.ListView):
+    template_name = "finance_tracking/bookentry.html"
+    context_object_name = "latest_bookentry_list"
+
+    def get_queryset(self):
+        return BookEntry.objects.order_by("-entry_date")[:5]
+
+
+class AccountView(generic.ListView):
+    template_name = "finance_tracking/account.html"
+    context_object_name = "latest_account_list"
+
+    def get_queryset(self):
+        return Account.objects.order_by("account_name")
