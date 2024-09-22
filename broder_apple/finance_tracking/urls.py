2e967779ab2eb25518de6e117802451851611197
EdgarGlowbo <efqr811@gmail.com>
2024-09-22 16:13:18 -0600

    Using viewsets and routers

--- broder_apple/finance_tracking/urls.py
+++ broder_apple/finance_tracking/urls.py
@@ -1,25 +1,14 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r"users", views.UserViewSet, basename="user")
+router.register(r"transactions", views.TransactionViewSet, basename="transaction")
+router.register(r"accounts", views.AccountViewSet, basename="account")
+
 
 urlpatterns = [
-    path("", views.api_root),
-    path("finance_tracking/", views.TransactionList.as_view(), name="transaction-list"),
-    path(
-        "finance_tracking/<int:pk>/",
-        views.TransactionDetail.as_view(),
-        name="transaction-detail",
-    ),
-    path(
-        "finance_tracking/income/",
-        views.IncomeTransactionView.as_view(),
-        name="income-transaction",
-    ),
-    path("finance_tracking/account/", views.AccountList.as_view(), name="account-list"),
-    path(
-        "finance_tracking/account/<int:pk>/",
-        views.AccountDetail.as_view(),
-        name="account-detail",
-    ),
-    path("users/", views.UserList.as_view(), name="user-list"),
-    path("users/<int:pk>/", views.UserDetail.as_view(), name="user-detail"),
+    path("", include(router.urls)),
 ]
--- broder_apple/finance_tracking/views.py
+++ broder_apple/finance_tracking/views.py
@@ -1,11 +1,6 @@
-from django.views import generic
-from django.http import HttpResponse, JsonResponse
-from django.http import HttpResponse
-
-from rest_framework import status, mixins, generics, permissions
+from rest_framework import status, generics, permissions, viewsets
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
@@ -13,11 +8,10 @@ from finance_tracking.permissions import IsOwnerOrReadOnly
 
 from .serializers import (
     AccountSerializer,
-    BookEntrySerializer,
     TransactionSerializer,
     UserSerializer,
 )
-from .models import Transaction, BookEntry, Account
+from .models import Transaction, Account
 from django.contrib.auth.models import User
 
 from rest_framework.permissions import IsAuthenticated
@@ -34,72 +28,39 @@ def api_root(request, format=None):
     )
 
 
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class TransactionList(
-    generics.ListCreateAPIView,
-):
+class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class TransactionDetail(
-    generics.RetrieveUpdateDestroyAPIView,
-):
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-
-class BookEntryList(generics.ListCreateAPIView):
-    queryset = BookEntry.objects.all()
-    serializer_class = BookEntrySerializer
-
-
-class BookEntryDetail(
-    generics.RetrieveUpdateDestroyAPIView,
-):
-    queryset = BookEntry.objects.all()
-    serializer_class = BookEntrySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-
-class AccountList(generics.ListCreateAPIView):
-    queryset = Account.objects.all()
-    serializer_class = AccountSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class AccountDetail(
-    generics.RetrieveUpdateDestroyAPIView,
-):
-    queryset = Account.objects.all()
-    serializer_class = AccountSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-
-class IncomeTransactionView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, format=None):
+    @action(detail=False, methods=["post"], permission_classes=[IsAuthenticated])
+    def income(self, request, *args, **kwargs):
+        """
+        Create an income transaction.
+        """
         serializer = TransactionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(owner=request.user, type=Transaction.INCOME)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
